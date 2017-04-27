626a3e0072c0195cee07f4bad6f1e5e48ddf0086
9468lgy <835459050@qq.com>
2017-04-27 12:13:07 +0800

    提交文件

---
+++ app/src/main/java/lgy/com/ffmpegdemo/MainActivity.java
@@ -0,0 +1,65 @@
+package lgy.com.ffmpegdemo;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private String filePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        surfaceView = (SurfaceView) findViewById(R.id.surface);
+        filePath = getInnerSDCardPath() + "/test.mp4";
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    /**
+     * 获取内置SD卡路径
+     *
+     * @return
+     */
+    public String getInnerSDCardPath() {
+        return Environment.getExternalStorageDirectory().getPath();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public static native int play(String url, Object surface);
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                play(filePath, surfaceHolder.getSurface());
+            }
+        }).start();
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
